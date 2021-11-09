b2b82c3269fba2e0a90c2cfdfc7034023f9a1e5a
Xiong Shihao <shihao.xiong@icloud.com>
2021-11-08 22:31:14 -0500

    complete backend

---
+++ backend/gcs.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"context"
+	"io"
+
+	"cloud.google.com/go/storage"
+)
+
+const (
+	BUCKET_NAME = "around_storage"
+)
+
+func saveToGCS(r io.Reader, objectName string) (string, error) {
+	ctx := context.Background()
+
+	client, err := storage.NewClient(ctx)
+	if err != nil {
+		return "", err
+	}
+
+	object := client.Bucket(BUCKET_NAME).Object(objectName)
+	wc := object.NewWriter(ctx)
+	if _, err := io.Copy(wc, r); err != nil {
+		return "", err
+	}
+
+	if err := wc.Close(); err != nil {
+		return "", err
+	}
+
+	if err := object.ACL().Set(ctx, storage.AllUsers, storage.RoleReader); err != nil {
+		return "", err
+	}
+
+	attrs, err := object.Attrs(ctx)
+	if err != nil {
+		return "", err
+	}
+
+	return attrs.MediaLink, nil
+}
---
+++ backend/go.mod
@@ -0,0 +1,13 @@
+module main
+
+go 1.13
+
+require (
+	cloud.google.com/go/storage v1.18.2
+	github.com/auth0/go-jwt-middleware v1.0.1
+	github.com/form3tech-oss/jwt-go v3.2.5+incompatible
+	github.com/google/uuid v1.1.2
+	github.com/gorilla/mux v1.8.0
+	github.com/olivere/elastic/v7 v7.0.29
+	github.com/pborman/uuid v1.2.1
+)
